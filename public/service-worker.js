c20fbe6723d8494c3283713ea22a10c9a042baf1
Said Mg <saidmghabghab@gmail.com>
2021-03-24 21:58:39 -0400

    add the caches done

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,17 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
+
+// install
+self.addEventListener("install", function (evt) {
+  // pre cache image data
+
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});
