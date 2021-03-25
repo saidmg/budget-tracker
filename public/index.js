f87200714eb2be0b965a2aeb234cba4a2d251831
Said Mg <saidmghabghab@gmail.com>
2021-03-24 21:54:28 -0400

    fixed the manifest.json

--- public/index.js
+++ public/index.js
@@ -1,6 +1,16 @@
-let transactions = [];
-let myChart;
 
+
+
+ if ("serviceWorker" in navigator) {
+   window.addEventListener('load',()=>{
+    navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker registered successfully."))
+    .catch(error => console.log("Service Worker registration failed:", error));
+   })
+  
+  }
+  let transactions = [];
+  let myChart;
 fetch("/api/transaction")
   .then(response => {
     return response.json();
